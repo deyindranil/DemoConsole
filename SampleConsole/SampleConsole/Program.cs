8c5d87a12181ee45eb598ddf2eac62433005dbe0
deyindranil <indranil.dey@rbc.com>
2017-04-27 13:22:53 +0530

    new commit

--- SampleConsole/SampleConsole/Program.cs
+++ SampleConsole/SampleConsole/Program.cs
@@ -11,6 +11,7 @@ namespace SampleConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("This is a modification");
         }
     }
 }
